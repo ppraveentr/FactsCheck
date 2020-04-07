ecbc1b36b4d4ad72342b2e64cfcccd3fdc6c5003
PraveenP <ppraveentr@gmail.com>
2020-04-07 22:42:16 +0530

    Initial Commit

---
+++ FactsCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FactsCheck
+//
+//  Created by Praveen P on 07/04/20.
+//  Copyright © 2020 Praveen P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
